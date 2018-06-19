9fef2b0ecc5a4b148303e8f64f85d75c96d16241
Yin, Jianfei <jianfei.yin@accenture.com>
2018-06-19 21:45:29 +0800

    submit by jianfei

--- aitpapi/src/main/java/com/aitp/dlife/web/rest/util/PaginationUtil.java
+++ aitpapi/src/main/java/com/aitp/dlife/web/rest/util/PaginationUtil.java
@@ -20,22 +20,22 @@ public final class PaginationUtil {
 
         HttpHeaders headers = new HttpHeaders();
         headers.add("X-Total-Count", Long.toString(page.getTotalElements()));
-        String link = "";
-        if ((page.getNumber() + 1) < page.getTotalPages()) {
-            link = "<" + generateUri(baseUrl, page.getNumber() + 1, page.getSize()) + ">; rel=\"next\",";
-        }
-        // prev link
-        if ((page.getNumber()) > 0) {
-            link += "<" + generateUri(baseUrl, page.getNumber() - 1, page.getSize()) + ">; rel=\"prev\",";
-        }
-        // last and first link
-        int lastPage = 0;
-        if (page.getTotalPages() > 0) {
-            lastPage = page.getTotalPages() - 1;
-        }
-        link += "<" + generateUri(baseUrl, lastPage, page.getSize()) + ">; rel=\"last\",";
-        link += "<" + generateUri(baseUrl, 0, page.getSize()) + ">; rel=\"first\"";
-        headers.add(HttpHeaders.LINK, link);
+//        String link = "";
+//        if ((page.getNumber() + 1) < page.getTotalPages()) {
+//            link = "<" + generateUri(baseUrl, page.getNumber() + 1, page.getSize()) + ">; rel=\"next\",";
+//        }
+//        // prev link
+//        if ((page.getNumber()) > 0) {
+//            link += "<" + generateUri(baseUrl, page.getNumber() - 1, page.getSize()) + ">; rel=\"prev\",";
+//        }
+//        // last and first link
+//        int lastPage = 0;
+//        if (page.getTotalPages() > 0) {
+//            lastPage = page.getTotalPages() - 1;
+//        }
+//        link += "<" + generateUri(baseUrl, lastPage, page.getSize()) + ">; rel=\"last\",";
+//        link += "<" + generateUri(baseUrl, 0, page.getSize()) + ">; rel=\"first\"";
+//        headers.add(HttpHeaders.LINK, link);
         return headers;
     }
 
