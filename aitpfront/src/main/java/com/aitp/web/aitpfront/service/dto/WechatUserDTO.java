d65751c566f32d6e9d26be5e7f71a6cde58e7fc0
Maolei <l.mao@accenture.com>
2018-06-21 10:16:32 +0800

    Upload by l.mao

---
+++ aitpfront/src/main/java/com/aitp/web/aitpfront/service/dto/WechatUserDTO.java
@@ -0,0 +1,115 @@
+package com.aitp.web.aitpfront.service.dto;
+
+/**
+ * 微信用户信息
+ */
+public class WechatUserDTO {
+    /**
+     * 用户的唯一标识
+     */
+    private String openId;
+    /**
+     * 用户昵称
+     */
+    private String userName;
+    /**
+     * 用户的性别，值为1时是男性，值为2时是女性，值为0时是未知
+     */
+    private String sex;
+    /**
+     * 用户个人资料填写的省份
+     */
+    private String province;
+    /**
+     * 普通用户个人资料填写的城市
+     */
+    private String city;
+    /**
+     * 国家，如中国为CN
+     */
+    private String country;
+    /**
+     * 用户头像，最后一个数值代表正方形头像大小
+     */
+    private String headimgurl;
+    /**
+     * 用户特权信息，json 数组，如微信沃卡用户为（chinaunicom）
+     */
+    private String privilege;
+    /**
+     * 只有在用户将公众号绑定到微信开放平台帐号后，才会出现该字段。
+     */
+    private String unionid;
+
+    public String getOpenId() {
+        return openId;
+    }
+
+    public void setOpenId(String openId) {
+        this.openId = openId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getHeadimgurl() {
+        return headimgurl;
+    }
+
+    public void setHeadimgurl(String headimgurl) {
+        this.headimgurl = headimgurl;
+    }
+
+    public String getPrivilege() {
+        return privilege;
+    }
+
+    public void setPrivilege(String privilege) {
+        this.privilege = privilege;
+    }
+
+    public String getUnionid() {
+        return unionid;
+    }
+
+    public void setUnionid(String unionid) {
+        this.unionid = unionid;
+    }
+}
