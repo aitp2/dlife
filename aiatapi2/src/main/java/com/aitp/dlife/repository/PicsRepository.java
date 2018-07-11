c9d7de7ad931670c345e2e67ee14235ccf67cb9d
Havey <havey.ruilin.li@accenture.com>
2018-07-11 17:49:24 +0800

    fix image bug
    fix image bug

--- aiatapi2/src/main/java/com/aitp/dlife/repository/PicsRepository.java
+++ aiatapi2/src/main/java/com/aitp/dlife/repository/PicsRepository.java
@@ -19,4 +19,8 @@ public interface PicsRepository extends JpaRepository<Pics, Long> {
     @Query(value = "select pics from Pics pics "
         + "where pics.clockIn.id =:clockInId",nativeQuery = false)
     List<Pics> findPicsByClockInId(@Param("clockInId") Long clockInId);
+
+    @Query(value = "select pics from Pics pics "
+        + "where pics.fitnessActivity.id =:activityId",nativeQuery = false)
+    List<Pics> findByActivityId(@Param("activityId") Long activityId);
 }
