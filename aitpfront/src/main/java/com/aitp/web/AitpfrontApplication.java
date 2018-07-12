4739ce7bbafd86f00e263c0ce353859cc7e9e6d3
zach.liang <dong_liang@cathaypacific.com>
2018-07-12 15:43:52 +0800

    修改vue 为tomcat 支持。

--- aitpfront/src/main/java/com/aitp/web/AitpfrontApplication.java
+++ aitpfront/src/main/java/com/aitp/web/AitpfrontApplication.java
@@ -2,7 +2,7 @@ package com.aitp.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.embedded.undertow.UndertowServletWebServerFactory;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.ErrorPage;
 import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
@@ -13,7 +13,7 @@ public class AitpfrontApplication {
 
 	@Bean
     public ConfigurableServletWebServerFactory webServerFactory() {
-        UndertowServletWebServerFactory factory = new UndertowServletWebServerFactory();
+		TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
         factory.addErrorPages(new ErrorPage(HttpStatus.UNAUTHORIZED, "/"));
         return factory;
     }
