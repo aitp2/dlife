7d5b9024f73961b955947ffbcd03a99af636c33d
Justin20171102 <1066109082@qq.com>
2018-07-16 19:54:31 +0800

    update url for FitnessMessageServiceImpl.java

--- aitpfront/src/main/java/com/aitp/web/common/service/FitnessMessageServiceImpl.java
+++ aitpfront/src/main/java/com/aitp/web/common/service/FitnessMessageServiceImpl.java
@@ -64,7 +64,7 @@ public class FitnessMessageServiceImpl implements FitnessMessageService{
     public List<ActivityParticipationDTO> getActivityParticipationByActivityIdFromAPI(String id){
         final String restApiPath=env.getProperty("rest_api_url");
         List<ActivityParticipationDTO> dtos = new ArrayList<>();
-        String participationInfo = HttpUtil.doGetJson(restApiPath+"/fitness-activities/"+id);
+        String participationInfo = HttpUtil.doGetJson(restApiPath+"/activity-participations/getParticipationsByActivityId/"+id);
         if (StringUtils.isNotBlank(participationInfo)){
             Type listType = new TypeToken<ArrayList<ActivityParticipationDTO>>(){}.getType();
             Gson gson = new Gson();
