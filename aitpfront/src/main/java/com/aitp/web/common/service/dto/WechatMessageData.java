98926f579c975ec07b196b0d787e4bfcf7161b5c
zach.liang <dong_liang@cathaypacific.com>
2018-07-18 18:50:29 +0800

    修改消息信息。

---
+++ aitpfront/src/main/java/com/aitp/web/common/service/dto/WechatMessageData.java
@@ -0,0 +1,37 @@
+package com.aitp.web.common.service.dto;
+
+public class WechatMessageData {
+
+	private String keyName;
+	
+	private String value;
+	
+	private String color;
+
+	public String getKeyName() {
+		return keyName;
+	}
+
+	public void setKeyName(String keyName) {
+		this.keyName = keyName;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+	
+	
+	
+}
