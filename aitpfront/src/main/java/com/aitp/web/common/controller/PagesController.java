bf453e3423da52399b70d417d371135a141ca49c
Maolei <l.mao@accenture.com>
2018-08-12 02:25:17 +0800

    修复查看更多404错误

--- aitpfront/src/main/java/com/aitp/web/common/controller/PagesController.java
+++ aitpfront/src/main/java/com/aitp/web/common/controller/PagesController.java
@@ -9,12 +9,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class PagesController {
     Logger logger = LoggerFactory.getLogger(PagesController.class);
 
-    @RequestMapping(value = {"/pages/fit/index.html","/pages/fit/index.html/*","/pages/fit/index.html/*/*"})
+    @RequestMapping(value = {"/pages/fit/index.html","/pages/fit/index.html/*","/pages/fit/index.html/*/*","/pages/fit/index.html/*/*/*"})
     public String fitIndex(){
         return "pages/fit/index";
     }
 
-    @RequestMapping(value = {"/pages/pin/index.html","/pages/pin/index.html/*","/pages/pin/index.html/*/*"})
+    @RequestMapping(value = {"/pages/pin/index.html","/pages/pin/index.html/*","/pages/pin/index.html/*/*","/pages/pin/index.html/*/*/*"})
     public String pinIndex(){
         return "pages/pin/index";
     }
