a86d273231870ebdaee6b8cb7cb4b75b7c161979
Maolei <l.mao@accenture.com>
2018-08-26 13:52:57 +0800

    从front工程中移除前端相关的静态页面

--- aitpfront/src/main/java/com/aitp/web/common/controller/PagesController.java
+++ aitpfront/src/main/java/com/aitp/web/common/controller/PagesController.java
@@ -7,16 +7,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PagesController {
-    Logger logger = LoggerFactory.getLogger(PagesController.class);
-
-    @RequestMapping(value = {"/pages/fit/index.html","/pages/fit/index.html/*","/pages/fit/index.html/*/*","/pages/fit/index.html/*/*/*"})
-    public String fitIndex(){
-        return "pages/fit/index";
-    }
-
-    @RequestMapping(value = {"/pages/pin/index.html","/pages/pin/index.html/*","/pages/pin/index.html/*/*","/pages/pin/index.html/*/*/*"})
-    public String pinIndex(){
-        return "pages/pin/index";
-    }
-
+//    Logger logger = LoggerFactory.getLogger(PagesController.class);
+//    @RequestMapping(value = {"/pages/fit/index.html","/pages/fit/index.html/*","/pages/fit/index.html/*/*","/pages/fit/index.html/*/*/*"})
+//    public String fitIndex(){
+//        return "pages/fit/index";
+//    }
+//    @RequestMapping(value = {"/pages/pin/index.html","/pages/pin/index.html/*","/pages/pin/index.html/*/*","/pages/pin/index.html/*/*/*"})
+//    public String pinIndex(){
+//        return "pages/pin/index";
+//    }
 }
