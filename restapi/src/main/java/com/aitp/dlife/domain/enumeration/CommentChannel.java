3971d0cdbadc2f1c0c9974381a3985e0fc4c9898
acc_mingming <mingming.wang@accenture.com>
2018-09-26 23:54:17 +0800

    add POINT_PRODUCT for comment channel

--- restapi/src/main/java/com/aitp/dlife/domain/enumeration/CommentChannel.java
+++ restapi/src/main/java/com/aitp/dlife/domain/enumeration/CommentChannel.java
@@ -4,17 +4,15 @@ package com.aitp.dlife.domain.enumeration;
  * The CommentChannel enumeration.
  */
 public enum CommentChannel {
-    COOK, FIT, PIN, FAQS;
+    COOK, FIT, PIN, FAQS, POINT_PRODUCT;
 
+    public static CommentChannel valueIn(String code){
+        for (CommentChannel commentChannel : values()) {
+            if(commentChannel.toString().equals(code)){
+                return commentChannel;
+            }
+        }
+        return null;
+    }
 
-
- public static CommentChannel valueIn(String code){
-	 for (CommentChannel commentChannel : values()) {
-		if(commentChannel.toString().equals(code)){
-			return commentChannel;
-		}
-	}
-	 return null;
- }
-    
 }
