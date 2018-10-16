de97f5efa87d362a1f5d73f32cb485ef509afe79
kairong.su <kairong.su@accenture.com>
2018-10-16 10:21:28 +0800

    恢复接口token验证

--- restapi/src/main/java/com/aitp/dlife/config/SecurityConfiguration.java
+++ restapi/src/main/java/com/aitp/dlife/config/SecurityConfiguration.java
@@ -105,7 +105,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .antMatchers("/api/authenticate").permitAll()
             .antMatchers("/api/account/reset-password/init").permitAll()
             .antMatchers("/api/account/reset-password/finish").permitAll()
-            .antMatchers("/api/**").permitAll()
+            .antMatchers("/api/**").authenticated()
             .antMatchers("/management/health").permitAll()
             .antMatchers("/management/info").permitAll()
             .antMatchers("/management/**").hasAuthority(AuthoritiesConstants.ADMIN)
