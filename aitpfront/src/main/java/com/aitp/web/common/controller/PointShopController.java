1a98bc54bc78a9fce8a37a2f269b981cfe6e9cc8
acc_mingming <mingming.wang@accenture.com>
2018-10-17 02:00:26 +0800

    change the response about point shop send wechat messsage

--- aitpfront/src/main/java/com/aitp/web/common/controller/PointShopController.java
+++ aitpfront/src/main/java/com/aitp/web/common/controller/PointShopController.java
@@ -8,11 +8,15 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.MediaType;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @CrossOrigin
 @RestController
@@ -23,12 +27,15 @@ public class PointShopController {
 
     @Autowired
     private PointShopService pointShopService;
-
+    @ResponseBody
     @RequestMapping(path="/sendMessage",method=RequestMethod.POST, produces = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-    public String sendMessage(@RequestBody ActivityMessageDTO message){
+    public Map<String, Object> sendMessage(@RequestBody ActivityMessageDTO message){
         logger.info("request to send message for point user:{} type:{}",message.getTouser(),message.getType());
 
         pointShopService.sendMessage(message);
-        return "ok";
+        Map<String,Object> result = new HashMap<>();
+        result.put("accept",true);
+        result.put("message","消息已发送！");
+        return result;
     }
 }
